43b987b2c86eedaaf3fcd9a1d64e516979b9f70a
Sourav Suman <souravtyson@gmail.com>
2017-08-01 00:49:07 +0530

    This is a perl programming tutorial files

---
+++ Perl/first.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl       # known as shebang line. It is mostly common in unix-based scripting language.
+
+use 5.18.0;
+use warnings;
+
+print "Hello, world";
+say "Hello, World";
---
+++ Perl/second.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use 5.18.0;
+use warnings;
+
+my $fileName = "linesfile.txt";
+
+# scalar, array and hash - perl has three data types
+
+open(FH, $fileName);
+my @lines = <FH>;
+close(FH);
+
+my $count = scalar @lines;
+
+say "there are ". $count ." lines in ".$fileName;
